704366f554ca805870327371c1a7c3c2633708df
react4.smart <react.4@smartgate-egypt.com>
2024-06-08 16:12:15 +0200

    ggg

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,11 @@
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div
+        className={`h-[100vh] flex justify-center items-center bggg relative`}
+      >
+        <div className=" w-[500px] h-[600px] bg-blue-400 border sss"></div>
+      </div>
+    </>
+  );
 }
